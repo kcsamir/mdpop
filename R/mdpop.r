96fd8db7cb677a9a2a58095bb5ab30e213a9d7e1
kcsamir <kcsamir@gmail.com>
2021-06-29 12:49:10 +0200

    adsf

--- R/mdpop.r
+++ R/mdpop.r
@@ -1,6 +1,6 @@
 #' mdpop
 #'
-#' This package allows you to work with mdpop and has the list of functions
+#' This package allows you to work with mdpop and has the list of functions: cctoname, eapr, fread_all, fsaveVarCntry, funlinkcctext, loadx, cntocc, popinterpkc, popkc, propkc, pyramid, pyredu
 #' @param cctoname
 #' @param eapr
 #' @param fread_all
