5ed6a2753c1272d35a14f2fd570546ccb2891308
kcsamir <kcsamir@gmail.com>
2025-01-23 15:45:34 +0100

    merged eapr and eapr1

--- R/eapr.r
+++ R/eapr.r
@@ -1,13 +1,13 @@
 #' Convert given edu distribution into eapr
 #'
 #' This function returns EAPR for given education distribution
-#' @param prop distribution data 'edu' (sorted) 'value' for single age-group
-#' @return eapr
+#' @param prop distribution data 'edu' (sorted) 'value' as proportion for single age-group
+#' @return eapr datatable
 #' @keywords read
 #' @export
 #' @examples
 
-eapr <- function(prop) {
+eapr <- function(prop,edutcol=F) {
    prop <<- prop
    # stop("Hello")
   # print("Hello")
@@ -15,5 +15,6 @@ eapr <- function(prop) {
   n <- length(x)
   eapr.x <- prop[-nrow(prop), ]
   eapr.x$value <- x[2:n]/x[1:(n-1)]
+  if(edutcol) eapr.x[,edut:=paste0(edu,as.numeric(substr(edu,2,2))+1)]
   return(eapr.x)
 }
